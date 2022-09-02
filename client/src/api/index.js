0735b882f8a2552724b28214123eec9aa36a1a2e
Anudip Gupta <anudipdg2003@gmail.com>
2022-09-02 21:04:47 +0530

    Updates for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://restaurant5303.herokuapp.com/" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
--- server/index.js
+++ server/index.js
@@ -19,14 +19,14 @@ app.get("/", (req, res) => {
   res.send("App is running");
 });
 
-const PORT = process.env.PORT || 5000;
-
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port: ${process.env.PORT}`)
+    )
   )
   .catch((error) => console.log(mongoose.error));
