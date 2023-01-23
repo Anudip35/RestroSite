2d0fd2275ececa151c18784b95ef208ad62e115e
Anudip Gupta <anudipdg2003@gmail.com>
2023-01-23 14:36:57 +0530

    Changed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://restaurant5303.herokuapp.com/" });
+const API = axios.create({ baseURL: "https://restrosite.onrender.com/" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
